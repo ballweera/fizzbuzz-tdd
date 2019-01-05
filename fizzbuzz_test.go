fa7e6dab832c1a4588847f4614fc2bf77ff1f9c9
Weera Kasetsin <wkasetsin@gmail.com>
2019-01-05 11:41:11 +0700

    refactor: extract assert to function

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -6,34 +6,28 @@ import "github.com/ballweera/fizzbuzz-tdd"
 func TestFizzBuzzShouldSayOne(t *testing.T) {
 	got := fizzbuzz.Say(1)
 	want := "1"
-
-	if want != got {
-		t.Errorf("it should say %s but got %s", want, got)
-	}
+	assertError(want, got, t)
 }
 
 func TestFizzBuzzShouldSayTwo(t *testing.T) {
 	got := fizzbuzz.Say(2)
 	want := "2"
-
-	if want != got {
-		t.Errorf("it should say %s but got %s", want, got)
-	}
+	assertError(want, got, t)
 }
 
 func TestFizzBuzzShouldSayFizz(t *testing.T) {
 	got := fizzbuzz.Say(3)
 	want := "Fizz"
-
-	if want != got {
-		t.Errorf("it should say %s but got %s", want, got)
-	}
+	assertError(want, got, t)
 }
 
 func TestFizzBuzzShouldSaySix(t *testing.T) {
 	got := fizzbuzz.Say(6)
 	want := "Fizz"
+	assertError(want, got, t)
+}
 
+func assertError(want, got string, t *testing.T) {
 	if want != got {
 		t.Errorf("it should say %s but got %s", want, got)
 	}
