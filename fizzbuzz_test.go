597fbae22400062a1066bb68b9af2367f1fa3ae5
Weera Kasetsin <weera@TH-LW11400-M.local>
2019-01-05 11:25:48 +0700

    Fizzbuzz supports 1 and 2

---
+++ fizzbuzz_test.go
@@ -0,0 +1,22 @@
+package fizzbuzz_test
+
+import "testing"
+import "github.com/ballweera/fizzbuzz-tdd"
+
+func TestFizzBuzzShouldSayOne(t *testing.T) {
+	got := fizzbuzz.Say(1)
+	want := "1"
+
+	if want != got {
+		t.Errorf("it should say %s but got %s", want, got)
+	}
+}
+
+func TestFizzBuzzShouldSayTwo(t *testing.T) {
+	got := fizzbuzz.Say(2)
+	want := "2"
+
+	if want != got {
+		t.Errorf("it should say %s but got %s", want, got)
+	}
+}
